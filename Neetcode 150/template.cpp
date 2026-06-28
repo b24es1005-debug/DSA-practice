913423293f39600b2f37048a6a4561b0bcf1128e
Aadi <b24es1005@iitj.ac.in>
2026-06-28 12:15:48 +0530

    added template and twosum

---
+++ Neetcode 150/template.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    //function here
+
+};
+
+int main() {
+    Solution sol;
+
+    // Input
+
+    // Call function
+
+    // Print answer
+
+    return 0;
+}
+// Below is a two sum example
+
+/* #include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp; // value -> index
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if (mp.find(complement) != mp.end()) {
+                return {mp[complement], i};
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> ans = sol.twoSum(nums, target);
+
+    if (!ans.empty()) {
+        cout << "Indices: ";
+        for (int x : ans)
+            cout << x << " ";
+        cout << endl;
+    } else {
+        cout << "No solution found." << endl;
+    }
+
+    return 0;
+}*/
