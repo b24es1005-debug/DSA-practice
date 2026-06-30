6d8d5aedba11ff4e0cda8fd9de18f59e6f62f1fb
Aadi <b24es1005@iitj.ac.in>
2026-06-30 23:14:02 +0530

    crt LC 150

--- Neetcode 150/MEDIUM/reverse_polish_notation.cpp
+++ Neetcode 150/MEDIUM/reverse_polish_notation.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-
         stack<int> st;
 
         for (string s : tokens) {
 
-            if(s == '+' || s == '-' || s == '*' || s == '/'){
+            if(s == "+" || s == "-" || s == "*" || s == "/"){
                 int b = st.top();
                 st.pop();
                 int a = st.top();
                 st.pop();
-                if(s == '+') st.push(a+b);
-                else if(s == '-') st.push(a-b);
-                else if(s == '*') st.push(a*b);
-                else if(s == '/') st.push(a/b);
+                if(s == "+") st.push(a+b);
+                else if(s == "-") st.push(a-b);
+                else if(s == "*") st.push(a*b);
+                else if(s == "/") st.push(a/b);
             }
             else{//it must be operand only
                 st.push(stoi(s));//converting the string to int then pushing 
