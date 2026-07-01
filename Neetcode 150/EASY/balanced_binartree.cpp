f566b101cd7a2f1ad27e08bc1f203afbb8b6c5e9
Aadi <b24es1005@iitj.ac.in>
2026-07-01 16:03:07 +0530

    LC110

---
+++ Neetcode 150/EASY/balanced_binartree.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int height(TreeNode* root){
+        if(root == NULL) return 0;
+        return 1+max(height(root->left),height(root->right));
+    }
+    bool isBalanced(TreeNode* root) {
+        if(root == NULL) return true;
+        if(abs(height(root->left) - height(root->right)) <= 1 && isBalanced(root->left) && isBalanced(root->right)) return true;
+        return false;
+    }
+};
